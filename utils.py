c8d3ce9cd62b55ded44ba4ac02acecbf988b3963
root <root@PC89184.localdomain>
2021-05-02 23:33:16 +0800

    update the argument parser

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ import json
 
 def cli():
     ### parse arguments ###
-    parser = argparse.ArgumentParser(description="DeepRecBench")
+    parser = argparse.ArgumentParser(description="FastRec")
 
     # =========================================================================================
     # Model related configurations
@@ -65,17 +65,6 @@ def cli():
     parser.add_argument("--target_latency", type=float, default=10)
     parser.add_argument("--req_granularity", type=int, default=64)
 
-    parser.add_argument("--batch_configs", type=str, default="32-64-128-256-512-1024")
-    parser.add_argument("--tune_batch_qps", action="store_true", default=False)
-
-    parser.add_argument("--tune_accel_qps", action="store_true", default=False)
-    parser.add_argument("--accel_configs", type=str, default="128-256-512")
-
-    parser.add_argument("--stable_region", type=float, default=0.10)
-    parser.add_argument("--max_arr_range", type=float, default=100)
-    parser.add_argument("--min_arr_range", type=float, default=1)
-    parser.add_argument("--arr_steps", type=int, default=20)
-    parser.add_argument("--sched_timeout", type=int, default=100)
 
     # =========================================================================================
     # Hardware related configurations
